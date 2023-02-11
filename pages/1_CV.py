adb72f0c6cabd69021320e5e41d549c868a652b0
joao semeano <jsemeano@gmail.com>
2023-02-11 15:00:48 +0100

    cv section first push

---
+++ pages/1_CV.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import os
+
+st.set_page_config(
+    page_title='Joao Semeano CV',
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items=None)
+
+
+st.markdown('''
+            # Curriculum Vitae
+            In the sections to the left I share the detailed stages of my professional and education life
+            ''')
