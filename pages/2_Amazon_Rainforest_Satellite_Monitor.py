cd08e24d44ad0902f6503429caa8445b178bf35b
joao semeano <jsemeano@gmail.com>
2023-02-11 16:51:00 +0100

    picking long and lat with sliders

--- pages/2_Amazon_Rainforest_Satellite_Monitor.py
+++ pages/2_Amazon_Rainforest_Satellite_Monitor.py
@@ -1,6 +1,7 @@
 
 import streamlit as st
 import pandas as pd
+from datetime import date
 
 st.set_page_config(
     page_title='Amazon_alert',
@@ -11,11 +12,35 @@ st.set_page_config(
 
 
 
+col1, col2 = st.columns(2)
 
-lat = st.slider('Latitude - Y',  -9.0, -7.0, -8.0)
+# (max_items, cloud_cover,start_date,end_date,area)
 
+with col1:
+    cloud_cover = st.slider('Maximum cloud cover',  -9.0, -7.0, -8.0)
 
-lon = st.slider('Longitude - X', -74.0, -72.0, -73.0)
+with col2:
+    max_items = st.slider('Maximum number of images',  -9.0, -7.0, -8.0)
+
+col3, col4 = st.columns(2)
+
+with col3:
+    start_date = st.slider("When do you start?",
+                        value=datetime(2020, 1, 1),
+                        format="MM/DD/YY")
+
+with col4:
+    end_date = st.slider("When do you stop?",
+                        value=datetime(2020, 1, 2),
+                        format="MM/DD/YY")
+    
+    
+col5, col6 = st.columns(2)
+with col5:
+    lat = st.slider('Latitude - Y',  -9.0, -7.0, -8.0)
+
+with col6:
+    lon = st.slider('Longitude - X', -74.0, -72.0, -73.0)
 
 st.write(f"You picked a point at Latitude {lat} and Longitude {lon}.")
 
