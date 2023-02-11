661d71537d7a3f2b7702ce94fb74e6a29b7cd1df
joao semeano <jsemeano@gmail.com>
2023-02-11 21:12:45 +0100

    fix scale_values

--- pages/2_Amazon_Rainforest_Satellite_Monitor.py
+++ pages/2_Amazon_Rainforest_Satellite_Monitor.py
@@ -239,8 +239,8 @@ with col8:
             
             
 if show_mosaic == 1:
-    # st.write(mosaic_rgb)
-    st.write(mosaic_rgb.shape)
+    st.write(mosaic_rgb)
+    # st.write(mosaic_rgb.shape)
 
         
 
