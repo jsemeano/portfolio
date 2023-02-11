21c6b1d0a18754b4d413a1e7dda5f1a105f31aa9
joao semeano <jsemeano@gmail.com>
2023-02-11 15:37:41 +0100

    professional experience page - test icon

--- pages/1_CV.py
+++ pages/1_CV.py
@@ -26,7 +26,8 @@ with prof_tab:
     with st.expander('Freelance - Data Engineer'):
         st.markdown('''12/2022 - Current''')
         st.markdown(''':location: Berlin, Germany''')
-        st.markdown('''* Deforestation alert system : Leveraging of satellite imagery to give an NGO early warning of deforestation activities in the Amazon
+        st.markdown('''
+                    * Deforestation alert system : Leveraging of satellite imagery to give an NGO early warning of deforestation activities in the Amazon
                     * Responsible for API building and data infrastructure''')
 
 
