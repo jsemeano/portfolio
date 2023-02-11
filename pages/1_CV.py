b0c6d786bb636b5335c81e1161d04fefd7df30e8
joao semeano <jsemeano@gmail.com>
2023-02-11 15:30:59 +0100

    professional experience page - test icon

--- pages/1_CV.py
+++ pages/1_CV.py
@@ -17,25 +17,18 @@ st.markdown('''
 
 st.header('My NLP Job Hunt Tool')
 
-intro_tab, prof_tab, edu_tab, skill_tab, cert_tab = st.tabs(['Introduction', 'Professional experience', 'Education', 'Skills', 'Certificates'])
+prof_tab, edu_tab, skill_tab, cert_tab = st.tabs(['Professional experience', 'Education', 'Skills', 'Certificates'])
 
-with intro_tab:
-    st.markdown('''
-                #### This section is my inner tool for optimizing job applications.
-                * Upload your resume, and/or cover letter, and job description, and get back:
-                    * Gaps between requirements and candidate experience
-                    * Soft skills, hard skills and tools that were not mentioned in the documents
-                    * Ideas how to improve the douments to better apply for the job
-                    * General conclusion
-                * Upload the logo of the company, and extract main color palette from it to improve design.
-                * Use your openAI token or the password provided by me.
-                ''')
+
 
 with prof_tab:
 
-    with st.expander('Sketch to Image Transformer UI'):
-        st.markdown("""## Let's transform your sketches with our AI! :cat:""")
-        st.write('description bellow- streamlit canvas is not supported inside tabs')
+    with st.expander('# Freelance - Data Engineer'):
+        st.markdown(""" 12/2022 - Current
+                    :location: Berlin, Germany
+                    
+                    * Deforestation alert system : Leveraging of satellite imagery to give an NGO early warning of deforestation activities in the Amazon
+                    * Responsible for API building and data infrastructure""")
 
 
     # #image_choice = st.radio('I want to see similar:', ('photo', 'sketch'), horizontal = True)
