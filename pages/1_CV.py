8461f9c06352015beaa02e2c4caf67f83ac0401d
joao semeano <jsemeano@gmail.com>
2023-02-11 16:10:41 +0100

    professional experience page - first completion

--- pages/1_CV.py
+++ pages/1_CV.py
@@ -18,10 +18,25 @@ prof_tab, edu_tab, skill_tab, cert_tab = st.tabs(['Professional experience', 'Ed
 
 with prof_tab:
     
-    styled_text = '<span style="font-style: italic; color: red;">Freelance - Data Engineer</span>'
-    # st.write(styled_text, unsafe_allow_html=True)
 
-    with st.beta_expander(styled_text, unsafe_allow_html=True):
+    
+    # Define a custom CSS style for the expander header
+    st.set_config(
+        **{
+            "beta_expander": {
+                "header_expand_icon": '<svg style="color: red;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8 16l4-4 4 4z"/></svg>',
+                "header_collapse_icon": '<svg style="color: red;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16 8l-4 4-4-4z"/></svg>',
+                "header_font_size": "24px",
+                "header_font_weight": "bold",
+                "header_color": "blue",
+                "border_radius": "10px",
+                "background_color": "#f0f0f0",
+                "padding": "10px",
+            }
+        }
+    )
+
+    with st.beta_expander('Freelance - Data Engineer'):
 
         st.markdown(''':date: 12/2022 - Current''')
         st.markdown(''':round_pushpin: Berlin, Germany''')
