c1af4867b22b7ceceaaac4c19f58c5fc52e2c900
joao semeano <jsemeano@gmail.com>
2023-02-12 16:44:44 +0100

    seeing chip in streamlit

--- pages/2_Amazon_Rainforest_Satellite_Monitor.py
+++ pages/2_Amazon_Rainforest_Satellite_Monitor.py
@@ -214,7 +214,7 @@ with col8:
             
             
 if show_mosaic == 1:
-    st.image(mosaic_rgb)
+    st.image(mosaic_rgb, clamp=True, channels='RGB')
     st.write(mosaic_rgb.shape)
     # st.write(mosaic_rgb.iloc[0]['rgb'])
     # st.write(mosaic_rgb.iloc[0]['rgb'].shape)
