5e07f9660c56162a7d1e03cc32c9bff45c4e2a59
joao semeano <jsemeano@gmail.com>
2023-02-12 12:03:43 +0100

    chipping before scale_values

--- pages/2_Amazon_Rainforest_Satellite_Monitor.py
+++ pages/2_Amazon_Rainforest_Satellite_Monitor.py
@@ -250,7 +250,7 @@ with col8:
             
             
 if show_mosaic == 1:
-    st.write(mosaic_rgb.iloc[0])
+    st.write(mosaic_rgb.iloc[0]['rgb'])
     # st.write(mosaic_rgb.shape)
 
         
