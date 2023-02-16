f4a8c4303183b97588650304d4e3b37ed8d52035
joao semeano <jsemeano@gmail.com>
2023-02-16 11:36:21 +0100

    new introduction page

--- Introduction.py
+++ Introduction.py
@@ -23,24 +23,18 @@ col_num = 3
 
 #     return None
 
-st.header('I am not a front-end developer or a designer, but I still hope that this website will show you who I am.')
+# st.header('Hi! ')
 st.markdown('''
-            ### I am excited to start my data science path.
-            I chose to be in data science because for a tech profession with never-ending iner-disciplinary learning.
-            A Flexible, cross-industries possibilities.
-            And an ability to make a real impact, and possibilites in impact-tech.
-            ### I believe I am the right person for the job.
-            Agile, versitile and independent when it comes to learning new skills and abilities.
-            Proactive team member with the ability to walk the extra mile, lead and be led.
-            Data Science, cross-functionality teams and versitile learning are my passion.
-            Modesty and will to learn.
-            ### You will find here:
-            1. My current project- Amazon rainforest illegal activity monitor, that will actually be deployed in cooparation with NGOs from Peru, in order to assist them.
-            2. The final project in a machine learning program- A UI that allows you to sketch, and transform your sketch into an image, build with my amazing team at "Le Wagon".
-            3. A tool that I built for my self, based on GPT3, for optimizing job applications.
-            4. Just a short description of my current contract in NLP, because it is protected with an NDA.                5. Summary of my social enterprenaurship national projects.
-            6. Certifications, courses and University honors.
-            7. References.
+            # Hi! :hatching_chick: 
+            ## My name is João Domingues Semeano and I welcome you to my portfolio page
+            
+            ### After several years working in mechanical engineering and central banking, I decided to refocus on data science. 
+            
+            In this page you find:
+            1. My CV with professional experience, education and certifications
+            2. Sketch 2 Image: an app that searches for images similar to your sketch
+            3. Amazon Rainforest Satellite Monitor: an app built for an NGO to give early warning of deforestation activities in the Amazon
+
             ''')
 
 # with open("other/resume.pdf", "rb") as pdf_file:
