9d304ed945949d726280f227aa7db6d2044065f1
joao semeano <jsemeano@gmail.com>
2023-02-18 11:53:59 +0100

    TRY ANOTHER CERTIFICATE

--- pages/4_Certificates_and_references.py
+++ pages/4_Certificates_and_references.py
@@ -47,6 +47,7 @@ with edu_tab:
     # with st.expander('European Central Bank'):
         
     #     st.image(f'{base_path}{subfolder}Domingues Semeano_HR_EMPLOYMENT_LETTER_EN_V3-0001.jpg')
+    st.image(f'{base_url}{subfolder}catolica1.jpg', width=300, output_format='JPEG')
 
 
 
