5ccfde88e602d26322c7a5f5ffd233024c16a835
joao semeano <jsemeano@gmail.com>
2023-02-23 14:04:05 +0100

    streamlining chipping and scaling

--- pages/2_Amazon_Rainforest_Satellite_Monitor.py
+++ pages/2_Amazon_Rainforest_Satellite_Monitor.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 from datetime import date
 import numpy as np
-# import time
+import time
 from pystac_client import Client
 from shapely import geometry 
 import rioxarray
@@ -20,6 +20,7 @@ st.set_page_config(
 
 
 api_url = 'https://deforestation-2f7jkaqqnq-ew.a.run.app'
+api_url = 'http://localhost:80'
 prediction_url = api_url + '/classify'
 requests.get(api_url) #to activate container
 
@@ -43,36 +44,44 @@ requests.get(api_url) #to activate container
 
 def scale_values(values):
     # Get the minimum and maximum values
-    max_value_allowed = 5000
+    
     min_value = np.min(values)
     max_value = np.max(values)
+    if max_value > 5000:
+        max_value = 5000
+    
     # Calculate the range of the values
-    value_range = max_value_allowed - min_value
+    value_range = max_value - min_value
     # Scale the values to a range of 0 to 1
     scaled_values = np.array([255*((value - min_value) / value_range) for value in values]).astype(int)
     scaled_values = np.where(scaled_values >= 255, 255, scaled_values)
 
     return  scaled_values
 
-def chipping(mosaic,dist,overlap):
-
+def chiping_list(mosaic_shape,dist,overlap):
+    
     # calculate longitudes of top left corner    
-    vec_x = int((1-overlap)*dist)*np.arange(0,int(mosaic.shape[0]//int((1-overlap)*dist)),1)
-    vec_x[-1] = mosaic.shape[0]-dist
+    vec_x = int((1-overlap)*dist)*np.arange(0,int(mosaic_shape[1]//int((1-overlap)*dist)),1)
+    vec_x[-1] = mosaic_shape[1]-dist
 
     # calculate latitudes of top left corner
-    vec_y = int((1-overlap)*dist)*np.arange(0,int(mosaic.shape[1]//int((1-overlap)*dist)),1)    
-    vec_y[-1] = mosaic.shape[1]-dist
+    vec_y = int((1-overlap)*dist)*np.arange(0,int(mosaic_shape[2]//int((1-overlap)*dist)),1)    
+    vec_y[-1] = mosaic_shape[2]-dist
 
     # create the data frame with longitude and latitude of both corners of the chip
     chip_df = pd.DataFrame(data=np.array(np.meshgrid(vec_x,vec_y)).T.reshape(len(vec_x)*len(vec_y),2), columns=['x_top_left', 'y_top_left'])
 
     chip_df['x_bottom_right'] = chip_df.apply(lambda x: x['x_top_left']+dist , axis=1)
     chip_df['y_bottom_right'] = chip_df.apply(lambda x: x['y_top_left']+dist , axis=1)
-
-    chip_df['rgb'] = chip_df.apply(lambda x: mosaic[x['x_top_left']:x['x_bottom_right'],x['y_top_left']:x['y_bottom_right'],:]  , axis=1)
-
+    
     return chip_df
+
+def chipping(mosaic,chip_coord):
+    
+    # chip_df['rgb'] = chip_df.apply(lambda x: mosaic[x['x_top_left']:x['x_bottom_right'],x['y_top_left']:x['y_bottom_right'],:]  , axis=1)
+   
+    
+    return mosaic[chip_coord['x_top_left']:chip_coord['x_bottom_right'],chip_coord['y_top_left']:chip_coord['y_bottom_right']]
 
 def aws_sentinel_retrieve_item(max_items, cloud_cover,start_date,end_date,area):
     
@@ -100,6 +109,8 @@ def aws_sentinel_retrieve_item(max_items, cloud_cover,start_date,end_date,area):
 
 def aws_sentinel_chip(item,area):
     
+    scale_option = 1
+    
     geom = item.geometry['coordinates'][0]
     
     lon_geom = [i[0] for i in geom]
@@ -110,38 +121,75 @@ def aws_sentinel_chip(item,area):
     max_lat = max(lat_geom)
     min_lat = min(lat_geom)
     
-    ind_item = (-min_lon+area[0])//((-min_lon+max_lon)/61)*61 + (-min_lat+area[1])//((-min_lat+max_lat)/61)
-
+    ind_item = int((-min_lon+area[0])//((-min_lon+max_lon)/61)*61 + (-min_lat+area[1])//((-min_lat+max_lat)/61))
+    
     # Merge each colour band independently and group them into a RGB array
 
+    start = time.time()
+
     mosaic_red = rioxarray.open_rasterio(item.assets["B04"].href).values
+    
+    chip_list_df = chiping_list(mosaic_red.shape,256,0.3)
+
+    
+    chip_red = chipping(mosaic_red.reshape((mosaic_red.shape[1],mosaic_red.shape[2])),chip_list_df.iloc[ind_item])
+    
+    print(f'1st chip : {time.time()-start}')
+    
+    start = time.time()
+    
     mosaic_green = rioxarray.open_rasterio(item.assets["B03"].href).values
-    mosaic_blue = rioxarray.open_rasterio(item.assets["B02"].href).values  
 
-    mosaic_rgb = np.stack([mosaic_red.reshape((mosaic_red.shape[1],mosaic_red.shape[2])), 
-                           mosaic_green.reshape((mosaic_green.shape[1],mosaic_green.shape[2])), 
-                           mosaic_blue.reshape((mosaic_blue.shape[1],mosaic_blue.shape[2]))], 
+    chip_green = chipping(mosaic_green.reshape((mosaic_green.shape[1],mosaic_green.shape[2])),chip_list_df.iloc[ind_item])
+
+    print(f'2nd chip : {time.time()-start}')
+    
+    start = time.time()
+
+    mosaic_blue = rioxarray.open_rasterio(item.assets["B02"].href).values
+    
+    chip_blue = chipping(mosaic_blue.reshape((mosaic_blue.shape[1],mosaic_blue.shape[2])),chip_list_df.iloc[ind_item])
+
+    print(f'3rd chip : {time.time()-start}')
+    
+    if scale_option == 1:
+        
+        start = time.time()
+        
+        chip_red = scale_values(chip_red)
+        chip_green = scale_values(chip_green)
+        chip_blue = scale_values(chip_blue)
+        
+        print(f'scaling : {time.time()-start}')
+
+    # chip_green = chipping(mosaic_green,chip_list_df.iloc[ind_item])
+    # chip_blue = chipping(mosaic_blue,chip_list_df.iloc[ind_item])
+    
+    start = time.time()
+    mosaic_rgb = np.stack([chip_red, 
+                           chip_green, 
+                           chip_blue], 
                           axis=2)
+  
+    print(f'stacking chip : {start-time.time()}')
     
-    print(f'red : max - {np.max(mosaic_red)} , min - {np.min(mosaic_red)} ')
-    print(f'green : max - {np.max(mosaic_green)} , min - {np.min(mosaic_green)} ')
-    print(f'blue : max - {np.max(mosaic_blue)} , min - {np.min(mosaic_blue)} ')
+    # print(f'red : max - {np.max(mosaic_red)} , min - {np.min(mosaic_red)} ')
+    # print(f'green : max - {np.max(mosaic_green)} , min - {np.min(mosaic_green)} ')
+    # print(f'blue : max - {np.max(mosaic_blue)} , min - {np.min(mosaic_blue)} ')
     
-    del mosaic_blue
-    del mosaic_green
-    del mosaic_red
+    # del mosaic_blue
+    # del mosaic_green
+    # del mosaic_red
    
-    mosaic_rgb = chipping(mosaic_rgb,256,0.3)
 
-    mosaic_rgb = mosaic_rgb.iloc[int(ind_item)]['rgb']
+    # mosaic_rgb = mosaic_rgb.iloc[int(ind_item)]['rgb']
+
     
-    # print(mosaic_rgb)
+    # for i in range(3):
+    #     print(i)
+    #     mosaic_rgb[:,:,i] = scale_values(mosaic_rgb[:,:,i])
     
-    for i in range(3):
-        print(i)
-        mosaic_rgb[:,:,i] = scale_values(mosaic_rgb[:,:,i])
-    
-    return   mosaic_rgb 
+    return   mosaic_rgb
 
 
 ############### Streamlit page ####################
@@ -181,28 +229,36 @@ col7, col8, col9 = st.columns(3)
 show_mosaic = 100
 
 with col8:
-    st.map(lat_long_df)
+    # st.map(lat_long_df)
+    
+    start = time.time()
+    
     if st.button('Get chip'):
-        items =  aws_sentinel_retrieve_item(15, 70,start_date,end_date,[lon,lat])
+        item =  aws_sentinel_retrieve_item(15, 70,start_date,end_date,[lon,lat])
         
-        if items == 'failed':
+        if item == 'failed':
             show_mosaic = 0
             
         else:
-            print(items)
-            mosaic_rgb  = aws_sentinel_chip(items,[lon,lat])
+            # print(item)
+            mosaic_rgb  = aws_sentinel_chip(item,[lon,lat])
             show_mosaic = 1
 
     if show_mosaic == 1:
         # st.write(mosaic_rgb)
+        st.write(f'total time to get the chip : {((time.time()-start)/60):.3f}')
         st.image(mosaic_rgb, clamp=True, channels='RGB')
+        
+        start = time.time()
         
         sketch_byte = mosaic_rgb.tobytes()
         response_classes = requests.post(prediction_url, files={'sketch': sketch_byte}).json()
 
-        # prediction = np.array(response_classes)
+        prediction = np.array(response_classes)
         
-        st.write(response_classes)
+        st.write(prediction)
+        
+        st.write(f'running model : {time.time()-start}')
         
     elif show_mosaic == 0:
         st.write('No image returned. Please increase the considered period (start and end dates) or allow for more clouds (Maximum cloud cover).')
